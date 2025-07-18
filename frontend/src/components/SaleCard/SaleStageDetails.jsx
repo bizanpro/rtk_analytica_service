55eae22d6471d8379f024011cf2a2b3f17115337
Владислав Ухлин <heavybird@MacBook-Pro-Vladislav.local>
2025-07-18 16:18:09 +0300

    Доработка отображения детализации этапа в карточке продаж

--- frontend/src/components/SaleCard/SaleStageDetails.jsx
+++ frontend/src/components/SaleCard/SaleStageDetails.jsx
@@ -4,97 +4,106 @@ import formatMoney from "../../utils/formatMoney";
 const SaleStageDetails = ({ stageMetrics, setStageMetrics, mode }) => {
     return (
         <div className="flex flex-col gap-4">
-            <div className="flex items-center gap-4">
-                <div className="flex flex-col gap-2 flex-grow">
-                    <span className="flex items-center gap-2 text-gray-400">
-                        Стоимость предложения, руб.
-                        <span className="flex items-center justify-center border border-gray-300 p-1 rounded-[50%] w-[20px] h-[20px]">
-                            ?
+            {stageMetrics.name?.toLowerCase() !== "получен запрос" && (
+                <div className="flex items-center gap-4">
+                    <div className="flex flex-col gap-2 flex-grow">
+                        <span className="flex items-center gap-2 text-gray-400">
+                            Стоимость предложения, руб.
+                            <span className="flex items-center justify-center border border-gray-300 p-1 rounded-[50%] w-[20px] h-[20px]">
+                                ?
+                            </span>
                         </span>
-                    </span>
 
-                    <div className="grid grid-cols-[50px_1fr] items-center gap-4">
-                        <span> ФТА:</span>
+                        <div className="grid grid-cols-[50px_1fr] items-center gap-4">
+                            <span> ФТА:</span>
 
-                        <div className="border-2 border-gray-300 py-1 px-2">
-                            <input
-                                type="text"
-                                className="w-full"
-                                value={
-                                    formatMoney(stageMetrics.fta_value) || ""
-                                }
-                                onChange={(evt) => {
-                                    setStageMetrics((prev) => ({
-                                        ...prev,
-                                        fta_value: evt.target.value,
-                                    }));
-                                }}
-                                disabled={mode == "read"}
-                            />
+                            <div className="border-2 border-gray-300 py-1 px-2">
+                                <input
+                                    type="text"
+                                    className="w-full"
+                                    value={
+                                        formatMoney(stageMetrics.fta_value) ||
+                                        ""
+                                    }
+                                    onChange={(evt) => {
+                                        setStageMetrics((prev) => ({
+                                            ...prev,
+                                            fta_value: evt.target.value,
+                                        }));
+                                    }}
+                                    disabled={mode == "read"}
+                                />
+                            </div>
+                        </div>
+
+                        <div className="grid grid-cols-[50px_1fr] items-center gap-4">
+                            <span>ТК:</span>
+
+                            <div className="border-2 border-gray-300 py-1 px-2">
+                                <input
+                                    type="text"
+                                    className="w-full"
+                                    value={stageMetrics.tk_value || ""}
+                                    onChange={(evt) => {
+                                        setStageMetrics((prev) => ({
+                                            ...prev,
+                                            tk_value: evt.target.value,
+                                        }));
+                                    }}
+                                    disabled={mode == "read"}
+                                />
+                            </div>
                         </div>
                     </div>
 
-                    <div className="grid grid-cols-[50px_1fr] items-center gap-4">
-                        <span>ТК:</span>
+                    <div
+                        className="flex flex-col gap-2"
+                        style={{ flex: "0 0 60px" }}
+                    >
+                        <span className="flex items-center gap-2 text-gray-400">
+                            Изменение
+                            <span className="flex items-center justify-center border border-gray-300 p-1 rounded-[50%] w-[20px] h-[20px]">
+                                ?
+                            </span>
+                        </span>
 
-                        <div className="border-2 border-gray-300 py-1 px-2">
-                            <input
-                                type="text"
-                                className="w-full"
-                                value={stageMetrics.tk_value || ""}
-                                onChange={(evt) => {
-                                    setStageMetrics((prev) => ({
-                                        ...prev,
-                                        tk_value: evt.target.value,
-                                    }));
-                                }}
-                                disabled={mode == "read"}
-                            />
+                        <div
+                            className={`flex items-center border-2 border-gray-300 py-1 px-2 h-[30px] ${
+                                stageMetrics.fta_change_percent
+                                    ? pepcentColorHandler(
+                                          stageMetrics.fta_change_percent
+                                      )
+                                    : ""
+                            }`}
+                        >
+                            {stageMetrics.fta_change_percent || 0}%
+                        </div>
+
+                        <div
+                            className={`flex items-center border-2 border-gray-300 py-1 px-2 h-[30px] ${
+                                stageMetrics.tk_change_percent
+                                    ? pepcentColorHandler(
+                                          stageMetrics.tk_change_percent
+                                      )
+                                    : ""
+                            }`}
+                        >
+                            {stageMetrics.tk_change_percent || 0}%
                         </div>
                     </div>
                 </div>
-
-                <div
-                    className="flex flex-col gap-2"
-                    style={{ flex: "0 0 60px" }}
-                >
-                    <span className="flex items-center gap-2 text-gray-400">
-                        Изменение
-                        <span className="flex items-center justify-center border border-gray-300 p-1 rounded-[50%] w-[20px] h-[20px]">
-                            ?
-                        </span>
-                    </span>
-
-                    <div
-                        className={`flex items-center border-2 border-gray-300 py-1 px-2 h-[30px] ${
-                            stageMetrics.fta_change_percent
-                                ? pepcentColorHandler(
-                                      stageMetrics.fta_change_percent
-                                  )
-                                : ""
-                        }`}
-                    >
-                        {stageMetrics.fta_change_percent || 0}%
-                    </div>
-
-                    <div
-                        className={`flex items-center border-2 border-gray-300 py-1 px-2 h-[30px] ${
-                            stageMetrics.tk_change_percent
-                                ? pepcentColorHandler(
-                                      stageMetrics.tk_change_percent
-                                  )
-                                : ""
-                        }`}
-                    >
-                        {stageMetrics.tk_change_percent || 0}%
-                    </div>
-                </div>
-            </div>
+            )}
 
             <div className="flex flex-col gap-2">
-                <span className="text-gray-400">Комментарий:</span>
+                {stageMetrics.name?.toLowerCase() !== "получен запрос" && (
+                    <span className="text-gray-400">Комментарий:</span>
+                )}
                 <textarea
-                    className="border-2 border-gray-300 p-5 min-h-[300px]"
+                    className={`${
+                        stageMetrics.name?.toLowerCase() === "получен запрос"
+                            ? "p-3"
+                            : "border-2 border-gray-300 p-5"
+                    } min-h-[300px]`}
                     placeholder="Оставьте комментарий по этапу"
                     style={{ resize: "none" }}
                     value={stageMetrics.comment || ""}
