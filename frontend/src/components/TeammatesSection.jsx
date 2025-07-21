d737e0248e4588424f8cbc28454b66a2a18adfc3
Владислав Ухлин <heavybird@MacBook-Pro-Vladislav.local>
2025-07-21 14:06:36 +0300

    Редактор отчета. Добавлен поиск в блок исполнителя команды проекта

--- frontend/src/components/TeammatesSection.jsx
+++ frontend/src/components/TeammatesSection.jsx
@@ -1,3 +1,5 @@
+import CreatableSelect from "react-select/creatable";
+
 const TeammatesSection = ({
     index,
     handleTeammateChange,
@@ -7,34 +9,78 @@ const TeammatesSection = ({
     removeTeammate,
     mode,
 }) => {
+    let name;
+
+    if (mode == "read") {
+        name =
+            physicalPersons.length > 0 &&
+            physicalPersons.find(
+                (item) => item.id === person?.physical_person_id
+            ).name;
+    }
+
     return (
         <div className="grid items-center gap-3 grid-cols-2">
             <div className="flex flex-col gap-2 justify-between">
-                <div className="border-2 border-gray-300 p-1 h-[32px]">
-                    <select
-                        className="w-full h-full"
-                        value={person?.physical_person_id}
-                        onChange={(e) =>
-                            handleTeammateChange(
-                                index,
-                                "physical_person_id",
-                                Number(e.target.value)
-                            )
-                        }
-                        disabled={mode === "read" ? true : false}
-                    >
-                        <option value="0">Выберите сотрудника</option>
-                        {physicalPersons.map((person) => (
-                            <option value={person.id} key={person.id}>
-                                {person.name}
-                            </option>
-                        ))}
-                    </select>
+                <div
+                    className={`border-2 border-gray-300 flex items-center ${
+                        mode == "read" ? "h-[32px] p-1" : ""
+                    }`}
+                >
+                    {mode === "read" ? (
+                        <span
+                            className="whitespace-nowrap w-full truncate"
+                            title={name}
+                        >
+                            {name}
+                        </span>
+                    ) : (
+                        <CreatableSelect
+                            isClearable
+                            options={
+                                physicalPersons.length > 0 &&
+                                physicalPersons.map((item) => ({
+                                    value: item.id,
+                                    label: item.name,
+                                }))
+                            }
+                            className="w-full executor-block__name-field"
+                            placeholder="Выбрать исполнителя"
+                            noOptionsMessage={() => "Совпадений нет"}
+                            isValidNewOption={() => false}
+                            defaultValue={
+                                (physicalPersons.length > 0 &&
+                                    physicalPersons
+                                        .map((item) => ({
+                                            value: item.id,
+                                            label: item.name,
+                                        }))
+                                        .find(
+                                            (option) =>
+                                                option.value ===
+                                                person?.physical_person_id
+                                        )) ||
+                                null
+                            }
+                            onChange={(selectedOption) => {
+                                handleTeammateChange(
+                                    index,
+                                    "physical_person_id",
+                                    selectedOption.value
+                                );
+                            }}
+                            isDisabled={mode == "read"}
+                        />
+                    )}
                 </div>
             </div>
             <div className="flex items-center gap-3">
                 <div className="flex flex-col gap-2 justify-between flex-grow">
-                    <div className="border-2 border-gray-300 p-1 h-[32px]">
+                    <div
+                        className={`border-2 border-gray-300 p-1 ${
+                            mode == "read" ? "h-[32px]" : "h-[42px]"
+                        }`}
+                    >
                         <select
                             className="w-full h-full"
                             value={person?.role_id}
