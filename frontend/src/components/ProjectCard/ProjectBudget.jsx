f5d487a51d37d7cbab8e571a93b99bdc4a531cfd
Владислав Ухлин <heavybird@MacBook-Pro-Vladislav.local>
2025-07-30 14:09:32 +0300

    Карточка проекта. Доработка блоков бюджета проекта и срока реализации

--- frontend/src/components/ProjectCard/ProjectBudget.jsx
+++ frontend/src/components/ProjectCard/ProjectBudget.jsx
@@ -3,42 +3,41 @@ import getColorBySign from "../../utils/getColorBySign";
 const ProjectBudget = ({ projectData }) => {
     return (
         <div className="flex items-end gap-5 min-h-[42px]">
-            <div className="flex items-end gap-2">
-                {projectData?.project_budget !== null && (
-                    <>
+            {projectData?.project_budget !== null && (
+                <>
+                    <div className="flex items-end gap-2">
                         <strong className="text-4xl font-normal whitespace-nowrap">
                             {projectData.project_budget}
                         </strong>
                         <span className="text-1xl leading-6">
                             млрд <br /> руб
                         </span>
-                    </>
-                )}
-            </div>
-            <div className="flex flex-col gap-2">
-                {projectData.budget_difference_percentage !== null && (
-                    <div
-                        className={`flex gap-1 ${getColorBySign(
-                            projectData.budget_difference_percentage,
-                            "text-red-400",
-                            "text-green-400"
-                        )} max-w-[50px] overflow-hidden text-ellipsis whitespace-nowrap`}
-                    >
-                        {projectData.budget_difference_percentage}
-                        <span>%</span>
                     </div>
-                )}
 
-                {projectData?.fta_budget !== null && (
-                    <div className="flex gap-1">
-                        <div className="max-w-[50px] overflow-hidden text-ellipsis whitespace-nowrap">
-                            {projectData.fta_budget}
-                        </div>
+                    <div className="flex flex-col gap-2">
+                        {projectData.budget_difference_percentage !== null && (
+                            <div
+                                className={`flex gap-1 ${getColorBySign(
+                                    projectData.budget_difference_percentage,
+                                    "text-red-400",
+                                    "text-green-400"
+                                )} max-w-[50px] overflow-hidden text-ellipsis whitespace-nowrap`}
+                            >
+                                {projectData.budget_difference_percentage}
+                                <span>%</span>
+                            </div>
+                        )}
 
-                        <span>млрд</span>
+                        {projectData?.base_budget_for_difference !== null && (
+                            <div className="flex gap-1">
+                                {projectData.base_budget_for_difference}
+
+                                <span>млрд</span>
+                            </div>
+                        )}
                     </div>
-                )}
-            </div>
+                </>
+            )}
         </div>
     );
 };
--- frontend/src/components/ProjectCard/ProjectImplementationPeriod.jsx
+++ frontend/src/components/ProjectCard/ProjectImplementationPeriod.jsx
@@ -7,47 +7,51 @@ const ProjectImplementationPeriod = ({ projectData }) => {
 
     return (
         <div>
-            <div className="flex items-end gap-5 min-h-[42px] mb-2">
-                <div className="flex items-center gap-2">
-                    {projectData?.implementation_period > 0 && (
-                        <>
+            {projectData?.implementation_period !== null && (
+                <>
+                    <div className="flex items-end gap-5 min-h-[42px] mb-2">
+                        <div className="flex items-center gap-2">
                             <strong className="text-4xl font-normal">
                                 {projectData.implementation_period}
                             </strong>
                             <span className="text-1xl leading-6">мес.</span>
-                        </>
-                    )}
-                </div>
-
-                <div className="flex flex-col gap-2">
-                    {projectData?.implementation_period_difference_percentage !==
-                        null && (
-                        <div
-                            className={`flex gap-1 ${getColorBySign(
-                                projectData.implementation_period_difference_percentage,
-                                "text-red-400",
-                                "text-green-400"
-                            )}`}
-                        >
-                            <div className="max-w-[50px] overflow-hidden text-ellipsis whitespace-nowrap">
-                                {
-                                    projectData?.implementation_period_difference_percentage
-                                }
-                            </div>
-                            <span>%</span>
                         </div>
-                    )}
 
-                    {projectData?.fta_implementation_period > 0 && (
-                        <div className="flex gap-1">
-                            {+projectData.fta_implementation_period}
-                            <span>мес.</span>
+                        <div className="flex flex-col gap-2">
+                            {projectData?.implementation_period_difference_percentage !==
+                                null && (
+                                <div
+                                    className={`flex gap-1 ${getColorBySign(
+                                        projectData.implementation_period_difference_percentage,
+                                        "text-red-400",
+                                        "text-green-400"
+                                    )}`}
+                                >
+                                    <div className="max-w-[50px] overflow-hidden text-ellipsis whitespace-nowrap">
+                                        {
+                                            projectData?.implementation_period_difference_percentage
+                                        }
+                                    </div>
+                                    <span>%</span>
+                                </div>
+                            )}
+
+                            {projectData?.base_implementation_period_for_difference >
+                                0 && (
+                                <div className="flex gap-1">
+                                    {
+                                        projectData.base_implementation_period_for_difference
+                                    }
+                                    <span>мес.</span>
+                                </div>
+                            )}
                         </div>
+                    </div>
+
+                    {percentage && (
+                        <ProgressBlock percentage={percentage} date={date} />
                     )}
-                </div>
-            </div>
-            {percentage && (
-                <ProgressBlock percentage={percentage} date={date} />
+                </>
             )}
         </div>
     );
