45a01017c583d07dc4fc21d3dbb15392e75bbb4c
Владислав Ухлин <heavybird@MacBook-Pro-Vladislav.local>
2025-08-08 12:15:56 +0300

    Карточка проекта. Обновление дизайна

--- frontend/src/components/MultiSelect/MultiSelectField.jsx
+++ frontend/src/components/MultiSelect/MultiSelectField.jsx
@@ -13,6 +13,13 @@ const MultiSelectField = ({
 }) => {
     const [isActiveSelect, setIsActiveSelect] = useState("");
 
+    const selectedItems = selectedValues.map((item) =>
+        options.find((option) => option.value === item)
+    );
+
+    // console.log(selectedValues);
+    // console.log(selectedItems);
+
     return (
         <div className="form-multiselect">
             {isActiveSelect === target && (
@@ -22,12 +29,14 @@ const MultiSelectField = ({
                 />
             )}
 
-            <div
+            <button
+                type="button"
+                title="Выбрать из списка"
                 className="form-multiselect__field"
                 onClick={() => setIsActiveSelect(target)}
             >
                 {placeholder}
-            </div>
+            </button>
 
             {isActiveSelect === target && (
                 <MultiSelectWithSearch
@@ -37,6 +46,42 @@ const MultiSelectField = ({
                     fieldName={fieldName}
                     close={setIsActiveSelect}
                 />
+            )}
+
+            {selectedItems.length > 0 && (
+                <ul className="form-multiselect__list">
+                    {selectedItems.map((item) => (
+                        <li className="form-multiselect__item" key={item.value}>
+                            <span>{item.label}</span>
+
+                            <button
+                                type="button"
+                                title={`Удалить ${item.label} из списка`}
+                                onClick={() =>
+                                    onChange({
+                                        [fieldName]: selectedValues.filter(
+                                            (selectedValue) =>
+                                                selectedValue !== item.value
+                                        ),
+                                    })
+                                }
+                            >
+                                <svg
+                                    width="12"
+                                    height="12"
+                                    viewBox="0 0 12 12"
+                                    fill="none"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                        d="M7.06 6l2.652 2.652-1.06 1.06L6 7.061 3.348 9.712l-1.06-1.06L4.939 6 2.288 3.348l1.06-1.06L6 4.939l2.652-2.651 1.06 1.06L7.061 6z"
+                                        fill="#98A2B3"
+                                    />
+                                </svg>
+                            </button>
+                        </li>
+                    ))}
+                </ul>
             )}
         </div>
     );
