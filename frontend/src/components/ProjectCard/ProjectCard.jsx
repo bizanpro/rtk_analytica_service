e1b0fdb69f543acc5def80c7d834f0dd9b08be2a
Владислав Ухлин <heavybird@MacBook-Pro-Vladislav.local>
2025-10-17 10:55:18 +0300

    Новый компонент - мобильная навигация в карточках

--- frontend/src/components/ProjectCard/ProjectCard.jsx
+++ frontend/src/components/ProjectCard/ProjectCard.jsx
@@ -11,19 +11,26 @@ import { useWindowWidth } from "../../hooks/useWindowWidth.js";
 import ExecutorBlock from "../ExecutorBlock/ExecutorBlock";
 import EmptyExecutorBlock from "../ExecutorBlock/EmptyExecutorBlock";
 import ReportWindow from "../ReportWindow/ReportWindow.jsx";
+import CardBottomActions from "../CardBottomActions.js";
 import ProjectReportsList from "./ProjectReportsList.jsx";
+
 import ProjectStatisticsBlock from "./ProjectStatisticsBlock";
 import ProjectStatisticsBlockMobile from "./ProjectStatisticsBlockMobile";
+
 import ProjectTeam from "./ProjectTeam";
 import ReportServices from "./ReportServices";
 import ProjectImplementationPeriod from "./ProjectImplementationPeriod";
 import ProjectBudget from "./ProjectBudget";
 import AutoResizeTextarea from "../AutoResizeTextarea";
+
 import ManagementReportsTab from "../ManagementReportsTab/ManagementReportsTab";
 import ManagementReportsTabMobile from "../ManagementReportsTab/ManagementReportsTabMobile";
+
 import Hint from "../Hint/Hint";
+
 import CreatableSelect from "react-select/creatable";
 import CustomSelect from "../CustomSelect/CustomSelect";
+
 import BottomSheet from "../BottomSheet/BottomSheet";
 import BottomNavCard from "../BottomNav/BottomNavCard";
 import Popup from "../Popup/Popup.jsx";
@@ -1587,54 +1594,10 @@ const ProjectCard = () => {
                 </Popup>
             )}
 
-            <div className="card__bottom-actions">
-                <button
-                    type="button"
-                    title="Открыть отчёты"
-                    onClick={() => {
-                        setReportWindowsState(false);
-                        setActiveWindow("reports");
-                    }}
-                >
-                    <svg
-                        width="30"
-                        height="30"
-                        viewBox="0 0 30 30"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path
-                            d="M8.83116 9.62235H21.1633M8.83116 14.6909H21.1633M8.83116 19.4801H17.4517M6.06055 26.2405H24.0007C24.553 26.2405 25.0007 25.7928 25.0007 25.2405V4.75928C25.0007 4.20699 24.553 3.75928 24.0007 3.75928H6.06055C5.50826 3.75928 5.06055 4.20699 5.06055 4.75928V25.2405C5.06055 25.7928 5.50826 26.2405 6.06055 26.2405Z"
-                            stroke="#F38B00"
-                            strokeWidth="2"
-                        />
-                    </svg>
-                </button>
-
-                <button
-                    type="button"
-                    title="Открыть ОСВ"
-                    onClick={() => {
-                        setReportWindowsState(false);
-                        setActiveWindow("statistic");
-                    }}
-                >
-                    <svg
-                        width="30"
-                        height="30"
-                        viewBox="0 0 30 30"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path
-                            fillRule="evenodd"
-                            clipRule="evenodd"
-                            d="M10 21.25H6.25V18.75H10V16.25H6.25V13.75H10V3.75H18.75C20.4076 3.75 21.9973 4.40848 23.1694 5.58058C24.3415 6.75269 25 8.3424 25 10C25 11.6576 24.3415 13.2473 23.1694 14.4194C21.9973 15.5915 20.4076 16.25 18.75 16.25H12.5V18.75H21.25V21.25H12.5V26.25H10V21.25ZM18.75 13.75C19.7446 13.75 20.6984 13.3549 21.4016 12.6517C22.1049 11.9484 22.5 10.9946 22.5 10C22.5 9.00544 22.1049 8.05161 21.4017 7.34835C20.6984 6.64509 19.7446 6.25 18.75 6.25L12.5 6.25V13.75H18.75Z"
-                            fill="#F38B00"
-                        />
-                    </svg>
-                </button>
-            </div>
+            <CardBottomActions
+                setReportWindowsState={setReportWindowsState}
+                setActiveWindow={setActiveWindow}
+            />
 
             <BottomNavCard update={() => updateProject(projectId)}>
                 {mode == "edit" && availableToChange && (
--- frontend/src/components/SupplierCard/SupplierCard.jsx
+++ frontend/src/components/SupplierCard/SupplierCard.jsx
@@ -12,6 +12,7 @@ import { ToastContainer, toast } from "react-toastify";
 
 import CardProjects from "../CardProjects/CardProjects";
 import ReportWindow from "../ReportWindow/ReportWindow";
+import CardBottomActions from "../CardBottomActions.js";
 
 import SupplierStatisticBlock from "./SupplierStatisticBlock";
 import SupplierStatisticBlockMobile from "./SupplierStatisticBlockMobile";
@@ -797,53 +798,11 @@ const SupplierCard = () => {
                 </div>
             </section>
 
-            <div className="card__bottom-actions" ref={block7Ref}>
-                <button
-                    type="button"
-                    title="Открыть отчёты"
-                    onClick={() => {
-                        setReportWindowsState(false);
-                        setActiveWindow("reports");
-                    }}
-                >
-                    <svg
-                        width="30"
-                        height="30"
-                        viewBox="0 0 30 30"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path
-                            d="M8.83116 9.62235H21.1633M8.83116 14.6909H21.1633M8.83116 19.4801H17.4517M6.06055 26.2405H24.0007C24.553 26.2405 25.0007 25.7928 25.0007 25.2405V4.75928C25.0007 4.20699 24.553 3.75928 24.0007 3.75928H6.06055C5.50826 3.75928 5.06055 4.20699 5.06055 4.75928V25.2405C5.06055 25.7928 5.50826 26.2405 6.06055 26.2405Z"
-                            stroke="#F38B00"
-                            strokeWidth="2"
-                        />
-                    </svg>
-                </button>
-
-                <button
-                    type="button"
-                    title="Открыть ОСВ"
-                    onClick={() => {
-                        setReportWindowsState(false);
-                        setActiveWindow("statistic");
-                    }}
-                >
-                    <svg
-                        width="30"
-                        height="30"
-                        viewBox="0 0 30 30"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path
-                            fillRule="evenodd"
-                            clipRule="evenodd"
-                            d="M10 21.25H6.25V18.75H10V16.25H6.25V13.75H10V3.75H18.75C20.4076 3.75 21.9973 4.40848 23.1694 5.58058C24.3415 6.75269 25 8.3424 25 10C25 11.6576 24.3415 13.2473 23.1694 14.4194C21.9973 15.5915 20.4076 16.25 18.75 16.25H12.5V18.75H21.25V21.25H12.5V26.25H10V21.25ZM18.75 13.75C19.7446 13.75 20.6984 13.3549 21.4016 12.6517C22.1049 11.9484 22.5 10.9946 22.5 10C22.5 9.00544 22.1049 8.05161 21.4017 7.34835C20.6984 6.64509 19.7446 6.25 18.75 6.25L12.5 6.25V13.75H18.75Z"
-                            fill="#F38B00"
-                        />
-                    </svg>
-                </button>
+            <div ref={block7Ref}>
+                <CardBottomActions
+                    setReportWindowsState={setReportWindowsState}
+                    setActiveWindow={setActiveWindow}
+                />
             </div>
 
             <BottomNavCard update={() => updateData()}></BottomNavCard>
